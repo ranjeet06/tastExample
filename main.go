d2db2b89e0b2431c1122184e7ad7bf3cf4c1f7de
Ranjeet <Ranjeet33@gmail.com>
2022-01-31 17:44:38 +0530

    udated main.go file

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.GET("/books", controllers.FindBooks)
 	r.GET("/books/:id", controllers.FindBook)
 	r.POST("/books", controllers.CreateBook)
 	r.PATCH("/books/:id", controllers.UpdateBook)
 	r.DELETE("/books/:id", controllers.DeleteBook)
+	r.GET("/books", controllers.FindBooks)
 	err := r.Run("localhost:8080")
 	if err != nil {
 		panic("not run")
